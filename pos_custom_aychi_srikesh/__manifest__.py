f95f1872ad5b4ad1ee657e4a170bc209683f01aa
pedroguirao <pedro@serincloud.com>
2022-05-04 09:12:05 +0200

    aychi

--- pos_custom_aychi_srikesh/__manifest__.py
+++ pos_custom_aychi_srikesh/__manifest__.py
@@ -11,14 +11,14 @@
     "version": "14.0.1.0.0",
     "license": "LGPL-3",
     "depends": ["pos_restaurant",
-                #"pos_line_priority_srikesh"
+                "pos_line_priority_srikesh"
                 ],
     "data": [
         'views/pos_custom_templates.xml',
-        #'views/assets.xml',
-        #'views/pos_session.xml',
+        'views/assets.xml',
+        'views/pos_session.xml',
              ],
     "qweb": [
-        #"static/src/xml/multiprint.xml",
+        "static/src/xml/multiprint.xml",
              ],
 }
